02a904a7d771c79982214ef18fb4cc87948ea01f
Luis Montesino <luis.miguel.mc016@gmail.com>
2025-07-04 14:26:10 -0400

    Cree un __init__.py para usar la carpeta scripts como un paquete

---
+++ scripts/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+# Nombre: Luis Miguel Montesino Cedeño
+# Matrícula: 15-EISN-2-052
+# Inicializador del paquete scripts
+
+# Este archivo le indica a Python que la carpeta 'scripts' es un paquete.
+# Gracias a esto, se pueden importar sus módulos desde otros archivos del proyecto.
+# por ejemplo: from scripts.menu import mostrar_menu

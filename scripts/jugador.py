4e087c4a3e56f24e7f3b511337cf78eafc3a6491
Luis Montesino <luis.miguel.mc016@gmail.com>
2025-07-04 23:32:51 -0400

    Desarrolle la movilidad

--- scripts/jugador.py
+++ scripts/jugador.py
@@ -1,4 +1,23 @@
 # scripts/jugador.py
 # Nombre: Luis Miguel Montesino Cedeño
 # Matrícula: 15-EISN-2-052
-#Clase para control la nave por el jugador
+#Clase para controlar la nave por el jugador
+
+import pygame
+
+class NaveJugador(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        # Cargar imagen de la nave y establecer transparencia
+        self.imagen_original = pygame.image.load("assets/images/nave.png").convert_alpha()
+
+        # Ajusta este tamaño de la nave
+        self.imagen_original = pygame.transform.scale(self.imagen_original, (75,75))
+
+        self.image = self.imagen_original
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def update(self, teclas, mouse_pos):
+        # Actualizar la posición de la nave para que siga al mouse
+        self.rect.center = mouse_pos

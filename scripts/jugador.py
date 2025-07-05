c9702d5113afd49cdd98becb19de8af73ef19b4e
Luis Montesino <luis.miguel.mc016@gmail.com>
2025-07-05 08:47:13 -0400

    Se importo la clase Bala y se añadieron métodos disparar y dibujar balas en NaveJugador

--- scripts/jugador.py
+++ scripts/jugador.py
@@ -4,20 +4,41 @@
 #Clase para controlar la nave por el jugador
 
 import pygame
+from scripts.bala import Bala #Importamos la clase bala
 
 class NaveJugador(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
 
-        # Cargar imagen de la nave y establecer transparencia
+        # Cargar la imagen de la nave
         self.imagen_original = pygame.image.load("assets/images/nave.png").convert_alpha()
 
-        # Ajusta este tamaño de la nave
+        # Ajusta el tamaño de la nave
         self.imagen_original = pygame.transform.scale(self.imagen_original, (75,75))
 
+        # Asigna la imagen actual a mostrar y su posición inicial
         self.image = self.imagen_original
         self.rect = self.image.get_rect(center=(x, y))
 
+        # Grupo de sprite donde se guardan las balas disparadas
+        self.balas = pygame.sprite.Group() 
+
+
     def update(self, teclas, mouse_pos):
-        # Actualizar la posición de la nave para que siga al mouse
+        # Actualiza la posición de la nave para que siga al mouse
         self.rect.center = mouse_pos
+
+        # Actualiza la posicion de todas las balas disparadas
+        self.balas.update()##
+
+    def disparar(self):
+        # Crea una nueva bala en frente de la nave
+        bala = Bala(self.rect.centerx, self.rect.top)
+        self.balas.add(bala)
+
+    def dibujar_balas(self, pantalla):
+        # Dibuja todas las balas en la pantalla
+        self.balas.draw(pantalla)
+
+      
+        
